9d26e38e1880b761472d663740053323ecc11f14
Paul Mederos <paulmederos@me.com>
2013-05-15 19:01:48 -0400

    Coffeescript added. GA added to main script.

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,18 @@
+// Function to show the About section
+var showAbout = function showAbout(){
+  $('.about-me').addClass('animated fadeInDown').delay(500).fadeIn('slow', function(){
+    $('.find-me-button').show().addClass('animated fadeInDown');
+  });
+}
+
+// Google Analytics variables
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-30913790-1']);
+_gaq.push(['_setDomainName', 'paulmederos.com']);
+_gaq.push(['_trackPageview']);
+
+
+// Once page is loaded, run these scripts
 $(document).ready(function() {
   window.setTimeout(showAbout, 500);
 
@@ -6,10 +21,9 @@ $(document).ready(function() {
       $('.on-the-internet').show().addClass('animated bounceInUp');
     });
   });
-});
 
-var showAbout = function showAbout(){
-  $('.about-me').addClass('animated fadeInDown').delay(500).fadeIn('slow', function(){
-    $('.find-me-button').show().addClass('animated fadeInDown');
-  });
-}
+  // Google Analytics runtime
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+});
