ec8f4f020ffe826667cad06381adf89cdcca085d
Paul Mederos <paulmederos@me.com>
2013-05-15 13:28:44 -0400

    Wrapped up HTML and styling. Added images. Now it's time to refector ALL THE THINGS

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  window.setTimeout(showAbout, 500);
+
+  $('.find-me-button').click(function(){
+    $(this).addClass('fadeOutUp').fadeOut(500, function(){
+      $('.on-the-internet').show().addClass('animated bounceInUp');
+    });
+  });
 });
+
+var showAbout = function showAbout(){
+  $('.about-me').addClass('animated fadeInDown').delay(500).fadeIn('slow', function(){
+    $('.find-me-button').show().addClass('animated fadeInDown');
+  });
+}
